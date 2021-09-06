feda71e1e2fbdccfc6666db418a9de6fee2f3a46
Alex Diker <alex.diker@live.ca>
2021-09-06 11:59:12 -0400

    Add files via upload

---
+++ add-twonumbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        addens = l1, l2
+        dummyhead = end = ListNode()
+        while addens or carry:
+            carry += sum(a.val for a in addens)
+            if addens:
+                end.next = addens[0]
+                end.next.val = carry % 10
+                end = end.next
+                addens = [a.next for a in addens if a.next]
+            else:
+                end.next = ListNode(carry)
+            carry //= 10
+        return dummyhead.next
